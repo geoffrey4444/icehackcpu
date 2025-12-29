6e67bcdc1609d5f8279d5e0200ed3c654ae16766
Geoffrey Lovelace <geoffrey4444@gmail.com>
2025-12-29 10:07:10 -0500

    Add not1 gate

--- Chapter1/simplegates.v
+++ Chapter1/simplegates.v
@@ -4,15 +4,22 @@ module top(
   input wire CLK,
   input wire BTN1,
   input wire BTN2,
-  output wire LED1
+  output wire LED1,
+  output wire LED2
 );
 
 // Logic gates to build and test
 wire nand2out;
-nand2 gate1(
+nand2 my_nand2_gate(
   .a(BTN1),
   .b(BTN2),
   .y(nand2out)
+);
+
+wire not1out;
+not1 my_not1_gate(
+  .a(nand2out),
+  .y(not1out)
 );
 
 // Clock loop just to dim LEDs (only light 1/32nd of the time)
@@ -21,7 +28,9 @@ always @(posedge CLK) begin
   clk_leds <= clk_leds + 1;
 end
 
+// Set LED lights
 assign LED1 = (clk_leds < 8) & nand2out;
+assign LED2 = (clk_leds < 8) & not1out;
 endmodule  // top
 
 // nand2
@@ -31,4 +40,16 @@ module nand2(
   output wire y
 );
 assign y = ~(a & b);
+endmodule
+
+// not1
+module not1(
+  input wire a,
+  output wire y
+);
+nand2 u_nand2(
+  .a(a),
+  .b(a),
+  .y(y)
+);
 endmodule
