03e55db456b30fd0421d0b7254a3b8f6be27f96a
Geoffrey Lovelace <geoffrey4444@gmail.com>
2025-12-29 10:47:21 -0500

    Add more simple gates

--- Chapter1/simplegates.v
+++ Chapter1/simplegates.v
@@ -5,7 +5,10 @@ module top(
   input wire BTN1,
   input wire BTN2,
   output wire LED1,
-  output wire LED2
+  output wire LED2,
+  output wire LED3,
+  output wire LED4,
+  output wire LED5
 );
 
 // Logic gates to build and test
@@ -18,8 +21,30 @@ nand2 my_nand2_gate(
 
 wire not1out;
 not1 my_not1_gate(
-  .a(nand2out),
+  .a(BTN1),
   .y(not1out)
+);
+
+wire and2out;
+and2 my_and2_gate(
+  .a(BTN1),
+  .b(BTN2),
+  .y(and2out)
+);
+
+wire or2out;
+or2 my_or2_gate(
+  .a(BTN1),
+  .b(BTN2),
+  .y(or2out)
+);
+
+
+wire xor2out;
+xor2 my_xor2_gate(
+  .a(BTN1),
+  .b(BTN2),
+  .y(xor2out)
 );
 
 // Clock loop just to dim LEDs (only light 1/32nd of the time)
@@ -31,6 +56,9 @@ end
 // Set LED lights
 assign LED1 = (clk_leds < 8) & nand2out;
 assign LED2 = (clk_leds < 8) & not1out;
+assign LED3 = (clk_leds < 8) & and2out;
+assign LED4 = (clk_leds < 8) & or2out;
+assign LED5 = (clk_leds < 8) & xor2out;
 endmodule  // top
 
 // nand2
@@ -40,7 +68,7 @@ module nand2(
   output wire y
 );
 assign y = ~(a & b);
-endmodule
+endmodule  // nand2
 
 // not1
 module not1(
@@ -52,4 +80,72 @@ nand2 u_nand2(
   .b(a),
   .y(y)
 );
-endmodule
+endmodule  // not1
+
+// and2
+module and2(
+  input wire a,
+  input wire b,
+  output wire y
+);
+wire u_nand2_ab;
+nand2 u_nand2(
+  .a(a),
+  .b(b),
+  .y(u_nand2_ab)
+);
+not1 u_not1(
+  .a(u_nand2_ab),
+  .y(y)
+);
+endmodule  // and2
+
+// or2 = NAND(not(A),not(B))
+module or2(
+  input wire a,
+  input wire b,
+  output wire y
+);
+wire not_1a;
+not1 u_not1_a(
+  .a(a),
+  .y(not_1a)
+);
+wire not_1b;
+not1 u_not1_b(
+  .a(b),
+  .y(not_1b)
+);
+nand2 result(
+  .a(not_1a),
+  .b(not_1b),
+  .y(y)
+);
+endmodule  // or2
+
+// xor2 = AND(OR(A,B),NAND(A,B))
+module xor2(
+  input wire a,
+  input wire b,
+  output wire y
+);
+wire or_ab;
+or2 u_or_ab(
+  .a(a),
+  .b(b),
+  .y(or_ab)
+);
+
+wire nand_ab;
+nand2 u_nand_ab(
+  .a(a),
+  .b(b),
+  .y(nand_ab)
+);
+
+and2 u_result(
+  .a(or_ab),
+  .b(nand_ab),
+  .y(y)
+);
+endmodule  // xor2
