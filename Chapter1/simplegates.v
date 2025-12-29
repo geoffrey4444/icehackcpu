12b52944d9201273615a278873573d2fd0f8fc02
Geoffrey Lovelace <geoffrey4444@gmail.com>
2025-12-29 09:43:12 -0500

    Add nand2 gate module

--- Chapter1/simplegates.v
+++ Chapter1/simplegates.v
@@ -2,7 +2,17 @@
 
 module top(
   input wire CLK,
-  output wire LEDG_N
+  input wire BTN1,
+  input wire BTN2,
+  output wire LED1
+);
+
+// Logic gates to build and test
+wire nand2out;
+nand2 gate1(
+  .a(BTN1),
+  .b(BTN2),
+  .y(nand2out)
 );
 
 // Clock loop just to dim LEDs (only light 1/8th of the time)
@@ -10,7 +20,15 @@ reg [7:0] clk_leds = 0; // 8-bit clock for leds
 always @(posedge CLK) begin
   clk_leds <= clk_leds + 1;
 
-  LEDG_N = clk_leds & 1'b1;
+  LED1 <= (clk_leds == 0) & nand2out;
 end
 endmodule  // top
 
+// nand2
+module nand2(
+  input wire a,
+  input wire b,
+  output wire y
+);
+assign y = ~(a & b);
+endmodule
