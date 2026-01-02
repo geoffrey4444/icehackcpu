a3f37134e56b09bb9d9362af71c64c3a2a8acafb
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-01-02 14:58:22 -0500

    Add 16-bit register

--- memory/memory_dff.v
+++ memory/memory_dff.v
@@ -11,3 +11,18 @@ wire mux_out;
 mux u_mux(.a(out), .b(in), .sel(load), .y(mux_out));
 dff u_dff(.clock(clock), .in(mux_out), .out(out));
 endmodule  // bit
+
+// A 16-bit register
+module register16(
+  input wire clock,
+  input wire [15:0] in,
+  input wire load,
+  output wire [15:0] out
+);
+genvar i;
+generate
+  for (i = 0; i < 16; ++i) begin : gen_bits
+    bit_register u_bit_reg(.clock(clock), .in(in[i]), .load(load), .out(out[i]));
+  end
+endgenerate
+endmodule  //register16
--- memory/tb_memory_dff.v
+++ memory/tb_memory_dff.v
@@ -7,14 +7,21 @@ reg in = 0;
 reg load;
 wire out;
 reg run_clock = 1;
+reg run_clock16 = 1;
+
+reg [15:0] in16;
+reg [15:0] out16;
+reg load16;
 
 bit_register u_bit(.clock(clock), .in(in), .load(load), .out(out));
+register16 u_register16(.clock(clock), .in(in16), .load(load16), .out(out16));
 
 // postedge (clock from 0 to 1) at t=5ns, 15ns, 25ns, ...
 initial begin : clock_generate
-  while (run_clock) #5 clock = ~clock;
+  while (run_clock | run_clock16) #5 clock = ~clock;
 end
 
+// Test bit
 initial begin
   clock = 0;
   in = 0;
@@ -52,5 +59,40 @@ initial begin
   if (out != 1'b0) $fatal;
 
   run_clock = 0;
+end
+
+// Test register16
+initial begin
+  in16 = 16'b0;
+  load16 = 0;
+  #15;
+  @(posedge clock);
+  #1;
+  if (out16 != 16'b0) $fatal;
+  @(negedge clock);
+  in16 = $random;
+  load16 = 1;
+  if (out16 != 16'b0) $fatal;
+  @(posedge clock);
+  #1;
+  if (out16 != in16) $fatal;
+  @(negedge clock);
+  load16 = 0;
+  @(posedge clock);
+  #1;
+  if (out16 != in16) $fatal;
+  @(negedge clock);
+  in16 = 16'sd3456;
+  load16 = 0;
+  @(posedge clock);
+  #1;
+  if (out16 == in16) $fatal;
+  @(negedge clock);
+  load16 = 1;
+  @(posedge clock);
+  #1;
+  if (out16 != in16) $fatal;
+
+  run_clock16 = 0;
 end
 endmodule  // tb
