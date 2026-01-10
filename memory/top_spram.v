a18fdbf3cfa6b1422697273c9fc8311542942767
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-01-10 15:59:13 -0800

    Update spram demo to print bytes over uart tx

--- memory/top_spram.v
+++ memory/top_spram.v
@@ -7,15 +7,20 @@ module top(
   output wire LED3,
   output wire LED4,
   output wire LED5,
+  output wire TX
 );
 
 // States for my FSM
 localparam integer WRITE = 0;
-localparam integer READ_WAIT = 1;
-localparam integer LATCH = 2;
+localparam integer FIRST_CLOCK_AFTER_WRITE = 10;
+localparam integer SECOND_CLOCK_AFTER_WRITE = 11;
+localparam integer THIRD_CLOCK_AFTER_WRITE = 12;
+localparam integer FOURTH_CLOCK_AFTER_WRITE = 13;
+localparam integer LATCH_TO_LEDs = 2;
 localparam integer DONE = 3;
 
-wire [15:0] data_to_write = 16'h4;
+// Wires and registers
+wire [15:0] data_to_write = 16'hac58;
 wire [13:0] address = 14'h7;
 wire [15:0] spram_data_out; // driven by module; don't assign default value
 
@@ -29,7 +34,26 @@ assign LED3 = data_to_show[2];
 assign LED4 = data_to_show[3];
 assign LED5 = data_to_show[4];
 
-// insert SPRAM module here
+reg [7:0] other_byte = 8'b0;
+reg [7:0] pending_byte = 8'b0;
+reg byte_is_pending = 0;
+reg [31:0] bytes_sent = 0;  // big so I can read later on read many bytes
+wire baud_clock;  // driven by u_baud_tick
+wire valid = byte_is_pending;
+wire ready;  // driven by uart_tx
+
+// parts
+baud_tick u_baud_tick(.clock(CLK), .tick(baud_clock));
+
+uart_tx u_uart_tx(
+  .clock(CLK),
+  .baud_tick(baud_clock),
+  .byte_to_send(pending_byte),
+  .valid(valid),
+  .ready(ready),
+  .tx(TX)
+);
+
 SB_SPRAM256KA spram(
   .CLOCK(CLK),
   .CHIPSELECT(1'b1),
@@ -47,16 +71,39 @@ always @(posedge CLK) begin
   case (state)
     WRITE: begin
       data_to_show <= 16'b0;
-      state <= READ_WAIT;
+      state <= FIRST_CLOCK_AFTER_WRITE;
     end
-    READ_WAIT: begin
-      state <= LATCH;
+    FIRST_CLOCK_AFTER_WRITE: begin
+      // If uncommented below: output byte is 0x00
+      // pending_byte <= spram_data_out;
+      // byte_is_pending <= 1;       
+      state <= SECOND_CLOCK_AFTER_WRITE;
     end
-    LATCH: begin
+    SECOND_CLOCK_AFTER_WRITE: begin
+      // If uncommented below: output byte is 0x58 'X'
+      // This means when writing, out is the new byte 2 cycles later
+      // pending_byte <= spram_data_out;
+      // byte_is_pending <= 1;
+      state <= THIRD_CLOCK_AFTER_WRITE;
+    end
+    THIRD_CLOCK_AFTER_WRITE: begin
+      // Read the byte from RAM into another register
+      other_byte <= spram_data_out;
+      state <= FOURTH_CLOCK_AFTER_WRITE;
+    end
+    FOURTH_CLOCK_AFTER_WRITE: begin
+      // If uncommented below: output byte is 0x58 'X'
+      // This means that when reading, out is the new byte 1 cycle later
+      pending_byte <= other_byte;
+      byte_is_pending <= 1; 
+      state <= LATCH_TO_LEDs;
+    end
+    LATCH_TO_LEDs: begin         
       data_to_show <= spram_data_out;
       state <= DONE; 
     end
     DONE: begin
+      byte_is_pending <= 0;
       state <= DONE;
     end
   endcase
