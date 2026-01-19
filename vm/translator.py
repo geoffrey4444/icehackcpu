dd333abaa4d904f8a30851335a74010488bf884f
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-01-19 07:16:41 -0800

    Update translator to read multiple files

--- vm/translator.py
+++ vm/translator.py
@@ -100,7 +100,9 @@ def write_pushpop(command, segment, index, file_stem):
     if int(index) < 0:
         print(f"Error: {index} is negative")
         exit(1)
-    if (segment == "pointer" and int(index) > 1) or (segment == "temp" and int(index) > 7):
+    if (segment == "pointer" and int(index) > 1) or (
+        segment == "temp" and int(index) > 7
+    ):
         print(f"Error: cannot push {segment} {index} because index is out of range")
         exit(1)
     if segment == "uart" and index > 2:
@@ -356,6 +358,8 @@ M=M+1
             return "// not\n@SP\nA=M-1\nM=!M\n"
 
 
+# Note: LCL, ARG, THIS, THAT are made up addresses for testing.
+# In a real code, these pointers only take a valid value via call/return
 def write_prolog():
     return """
 // prolog
@@ -396,91 +400,93 @@ def write_epilog():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "input_file", type=str, help="Input file containing vack vm code"
+        "input_files", type=str, nargs="+", help="Input files containing vack vm code"
     )
     args = parser.parse_args()
 
-    # First, open the file
-    # list of vm commands
-    lines_to_parse = []
-    with open(args.input_file, "r") as input_file:
-        for line in input_file:
-            # remove leading and trailing whitespace
-            line = line.strip()
-            # ignore commented lines and empty lines, but add other lines for parsing
-            line = line.split("//", 1)[0].strip()
-            if not line:
-                continue
-            # convert tabs to spaces and remove newline characters
-            lines_to_parse.append(line.replace("\t", " ").replace("\n", ""))
-
     # Globals for label generation
-    # Variables for label generation
-
-    current_file_stem = Path(args.input_file).stem
     current_function = ""
     label_counter = 0
 
     assembly_code = write_prolog()
 
-    # Goal: generate hack assembly code corresponding to each line of vack
-    # virtual machine (vm) code. Must support the following instructions:
-    #  push segment index
-    #  pop segment index
-    #  add
-    #  sub
-    #  neg
-    #  eq
-    #  gt
-    #  lt
-    #  and
-    #  or
-    #  not
-    #
-    # segments include the following:
-    #  argument   function's argument variables
-    #  local      function's local variables
-    #  static     class variable associated with no particular object
-    #  constant   (virtual ... just implement as literals)
-    #  this
-    #  that
-    #  pointer
-    #  tmp
-    #  uart (tx = uart index 0, rx = uart index 1)
-    #
-    # Memory layout
-    #  0-16 registers
-    #    0 SP
-    #    1 LCL
-    #    2 ARG
-    #    3 THIS
-    #    4 THAT
-    # 5-12 temp
-    # 13-15 misc variables
-    #  16-255 static variables
-    #  256-2047 stack
-    #  24577 UART
-    #    UART[0] TX
-    #    UART[1] RX
-    #    UART[2] UARTSTAT (maybe omit??)
-    for line in lines_to_parse:
-        # figure out type of command
-        command_words = line.split()
-        command_name = command_words[0].lower()
-        if command_name in pushpop_commands:
-            segment = command_words[1].lower()
-            index = command_words[2]
-            assembly_code += write_pushpop(
-                command_name, segment, index, current_file_stem
-            )
-        elif command_name in arithmetic_commands:
-            assembly_code += write_arithmetic(
-                command_name, current_file_stem, label_counter
-            )
-            label_counter += 1
-        else:
-            print(f"Error: unknown command {command_name}")
-            exit(1)
+    # loop over input files
+    for input_file in args.input_files:
+        # First, open the file
+        # list of vm commands
+        lines_to_parse = []
+        with open(input_file, "r") as opened_input_file:
+            for line in opened_input_file:
+                # remove leading and trailing whitespace
+                line = line.strip()
+                # ignore commented lines and empty lines, but add other
+                # lines for parsing
+                line = line.split("//", 1)[0].strip()
+                if not line:
+                    continue
+                # convert tabs to spaces and remove newline characters
+                lines_to_parse.append(line.replace("\t", " ").replace("\n", ""))
+
+        current_file_stem = Path(input_file).stem
+
+        # Goal: generate hack assembly code corresponding to each line of vack
+        # virtual machine (vm) code. Must support the following instructions:
+        #  push segment index
+        #  pop segment index
+        #  add
+        #  sub
+        #  neg
+        #  eq
+        #  gt
+        #  lt
+        #  and
+        #  or
+        #  not
+        #
+        # segments include the following:
+        #  argument   function's argument variables
+        #  local      function's local variables
+        #  static     class variable associated with no particular object
+        #  constant   (virtual ... just implement as literals)
+        #  this
+        #  that
+        #  pointer
+        #  tmp
+        #  uart (tx = uart index 0, rx = uart index 1)
+        #
+        # Memory layout
+        #  0-16 registers
+        #    0 SP
+        #    1 LCL
+        #    2 ARG
+        #    3 THIS
+        #    4 THAT
+        # 5-12 temp
+        # 13-15 misc variables
+        #  16-255 static variables
+        #  256-2047 stack
+        #  24577 UART
+        #    UART[0] TX
+        #    UART[1] RX
+        #    UART[2] UARTSTAT (maybe omit??)
+        for line in lines_to_parse:
+            # figure out type of command
+            command_words = line.split()
+            command_name = command_words[0].lower()
+            if command_name in pushpop_commands:
+                segment = command_words[1].lower()
+                index = command_words[2]
+                assembly_code += write_pushpop(
+                    command_name, segment, index, current_file_stem
+                )
+            elif command_name in arithmetic_commands:
+                assembly_code += write_arithmetic(
+                    command_name, current_file_stem, label_counter
+                )
+                label_counter += 1
+            else:
+                print(f"Error: unknown command {command_name}")
+                exit(1)
 
     assembly_code += write_epilog()
     print(assembly_code)
