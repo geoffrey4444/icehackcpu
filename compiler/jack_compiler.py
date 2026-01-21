9d65813244c77176beb443ea50c909c06a359cdd
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-01-21 11:02:25 -0800

    Analyze terms and expressions

--- compiler/jack_compiler.py
+++ compiler/jack_compiler.py
@@ -55,6 +55,23 @@ symbols = [
     "~",
 ]
 
+binary_operators = [
+    "+",
+    "-",
+    "*",
+    "/",
+    "&",
+    "|",
+    "<",
+    ">",
+    "=",
+]
+
+unary_operators = [
+    "-",
+    "~",
+]
+
 whitespace_characters = [" ", "\t", "\n", "\r"]
 
 newline_characters = ["\n", "\r"]
@@ -126,7 +143,7 @@ class ArrayAccess:
     # a token of type identifier specifying the array name
     array_name_token: Token
     # an expression specifying the index
-    expression: Expression
+    array_index: Expression
 
 
 @dataclass
@@ -442,7 +459,177 @@ def xml_from_token_list(tokens):
     return xml_string
 
 
-# Functions and dataclasses representing program structure
+# Class for compiling program structure
+
+
+@dataclass
+class JackCompiler:
+    tokens: list[Token]
+    current_token_index: int = 0
+
+    def current_token(self) -> Token:
+        return self.tokens[self.current_token_index]
+
+    def next_token(self) -> Token:
+        if self.has_next_token():
+            return self.tokens[self.current_token_index + 1]
+        else:
+            raise ValueError("Next token requested, but no next token available")
+
+    def has_next_token(self) -> bool:
+        return self.current_token_index + 1 < len(self.tokens)
+
+    def advance(self):
+        self.current_token_index += 1
+
+    # term: integerConstant | stringConstant | keywordConstant | varName
+    # | varName '[' expression']' | '(' expression ')' | (unaryOp term)
+    # | subroutineCall
+    def compile_term(self) -> Term:
+        # current token determines term type, expect when we must also look
+        # ahead one token
+        current_token = self.current_token()
+        if current_token.type == "integerConstant":
+            self.advance()
+            return IntegerConstant(token=current_token)
+        elif current_token.type == "stringConstant":
+            self.advance()
+            return StringConstant(token=current_token)
+        elif current_token.type == "keyword":
+            if current_token.value in keyword_constants:
+                self.advance()
+                return KeywordConstant(token=current_token)
+            else:
+                raise ValueError(
+                    f"Keyword {current_token.value} is not a keyword constant"
+                )
+        elif current_token.type == "symbol":
+            if current_token.value == "(":
+                self.advance()  # advance past "("
+                result = ParentheticalExpression(expression=self.compile_expression())
+                if self.current_token().value == ")":
+                    self.advance()
+                    return result
+                else:
+                    raise ValueError(
+                        "Expected closing ) after ( in paranethetical expression"
+                    )
+            elif current_token.value in unary_operators:
+                unary_op_token = current_token
+                self.advance()
+                return UnaryOpTerm(
+                    op_token=unary_op_token, term_to_operate_on=self.compile_term()
+                )
+            else:
+                raise ValueError(f"Unexpected symbol {current_token.value} in term")
+        elif current_token.type == "identifier":
+            # Need to look ahead 1 token to decide what's next
+            # Possibilities:
+            #   1. Next token is '[' -- ArrayAccess
+            #   2. Next token is '(' -- SubroutineCall
+            #   3. Next token is not a symbol -- VarName
+            if not (self.has_next_token()):
+                self.advance()
+                return VarName(token=current_token)
+            if self.next_token().type != "symbol":
+                self.advance()  # advance past identifier token
+                return VarName(token=current_token)
+            else:
+                if self.next_token().value == "[":
+                    array_name_token = current_token
+                    self.advance()  # consumed identifier; current token is now "["
+                    if self.current_token().value != "[":
+                        raise ValueError(
+                            f"Expected [ after identifier {current_token.value} in array access"
+                        )
+                    self.advance()  # consumed "("; current token is now first token of expression
+                    array_index = self.compile_expression()
+                    if self.current_token().value == "]":
+                        self.advance()
+                        return ArrayAccess(
+                            array_name_token=array_name_token, array_index=array_index
+                        )
+                    else:
+                        raise ValueError("Expected closing ] after [ in array access")
+                elif self.next_token().value == "(":
+                    return self.compile_subroutine_call()
+                elif self.next_token().value == ".":
+                    return self.compile_subroutine_call()
+                else:
+                    raise ValueError(
+                        f"Unexpected symbol {self.next_token().value} after identifier {current_token.value}"
+                    )
+
+    def compile_subroutine_call(self) -> SubroutineCall:
+        subroutine_name_token = self.current_token()
+        receiver_name_token = None
+        if subroutine_name_token.type != "identifier":
+            raise ValueError(
+                f"Subroutine name token {subroutine_name_token.value} is type {subroutine_name_token.type}, not identifier"
+            )
+        self.advance()
+        # Two possibilities:
+        #  1. Next token is "(" -- implicitly call method of this
+        #  2. Next token is "." -- call method of class or object
+        if self.current_token().value == ".":
+            self.advance()  # advance past "."
+            receiver_name_token = subroutine_name_token
+            subroutine_name_token = self.current_token()
+            if subroutine_name_token.type != "identifier":
+                raise ValueError(
+                    f"Subroutine name token {subroutine_name_token.value} is type {subroutine_name_token.type}, not identifier"
+                )
+            self.advance()  # advance past subroutine name token
+        if self.current_token().value == "(":
+            self.advance()  # advance past "("
+            if self.current_token().value == ")":
+                self.advance()  # advance past ")"
+                return SubroutineCall(
+                    subroutine_name_token=subroutine_name_token,
+                    receiver_name_token=receiver_name_token,
+                    expression_list=ExpressionList(expressions=[]),
+                )
+            else:
+                expression_list = self.compile_expression_list()
+                if self.current_token().value != ")":
+                    raise ValueError(
+                        "Expected closing ) after ( in expression list in subroutine call"
+                    )
+                self.advance()  # advance past ")"
+                return SubroutineCall(
+                    subroutine_name_token=subroutine_name_token,
+                    receiver_name_token=receiver_name_token,
+                    expression_list=expression_list,
+                )
+        else:
+            raise ValueError(
+                f"Expected ( after subroutine name {subroutine_name_token.value}"
+            )
+
+    # Don't call this function if the expression list is empty
+    # Insead, in what would have bene the caller, recognize the list
+    # is empty (e.g. by finding ")" right away)
+    def compile_expression_list(self) -> ExpressionList:
+        expressions = [self.compile_expression()]
+        while self.current_token().value == ",":
+            self.advance()  # advance past ","
+            expressions.append(self.compile_expression())
+        return ExpressionList(expressions=expressions)
+
+    def compile_expression(self) -> Expression:
+        # Compile term and advance to next token not part of that term
+        # Note: compile_term() advances to appropriate token
+        expression = Expression(first_term=self.compile_term())
+        while (
+            self.current_token().type == "symbol"
+            and self.current_token().value in binary_operators
+        ):
+            operator_token = self.current_token()
+            self.advance()
+            next_term = self.compile_term()
+            expression.other_terms.append((operator_token, next_term))
+        return expression
+
 
 # main function: drive compilation
 
